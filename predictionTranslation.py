351399b67a19b7c80cf68296516524cf0a297ef7
carlosla2802 <133142194+carlosla2802@users.noreply.github.com>
2023-07-09 13:30:01 +0200

    Update predictionTranslation.py

--- predictionTranslation.py
+++ predictionTranslation.py
@@ -17,8 +17,7 @@ elif maquina == "Windows":
 else:
     filename = "/Users/carlosletaalfonso/github-classroom/DCC-UAB/xnap-project-ed_group_03/output/char2encoding.pkl" #### local leta
 
-sentence="Cat"
-#saveChar2encoding("char2encoding.pkl",input_token_index,16,71,reverse_target_char_index,num_decoder_tokens,target_token_index)
+sentence="Go away!"
 input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index= getChar2encoding(filename)
 encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,16,91) #(sentence,input_token_index,16,71)
 encoder_model= load_model('encoder_modelPredTranslation.h5')
